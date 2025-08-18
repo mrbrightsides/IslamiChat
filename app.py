7ff820d061d387c7c5f65259c859ce42766e2e37
Akhmad Khudri <khudri@binadarma.ac.id>
2025-08-18 23:27:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ from components.murottal import (
 
 # ===== Komponen: Khutbah GPT =====
 from components.khutbah_gpt import (
-    render_khutbah_form, generate_khutbah_gpt
+    render_khutbah_form, generate_khutbah
 )
 
 # ===== Page setup =====
